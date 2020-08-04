c8964365bd4a6f013d34bfe5faebd38cb6c52320
rrphl <raphaelong@me.com>
2020-08-05 00:09:03 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -5,7 +5,7 @@ MemberStack.onReady.then(async function(member) {
         var saved = metadata.saved;
         console.log(saved);
         if (metadata.saved) {
-            document.querySelectorAll("#iise .brands-item").forEach(function(element) {
+            document.querySelectorAll("#iise .saved-item").forEach(function(element) {
                 var link = $(".saved-item-info-link");
                 console.log(link);
             });

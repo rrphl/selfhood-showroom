4382f7d5e069d3cdc41aec0b57749d7190fbc828
rrphl <raphaelong@me.com>
2021-02-23 06:45:32 +0800

    add

--- js/test.js
+++ js/test.js
@@ -5,9 +5,11 @@ MemberStack.onReady.then(async function(member) {
         var saved = metadata.saved;
         console.log(saved);
         if (metadata.saved) {
-            document.querySelectorAll("#iise .saved-item .saved-item-name").forEach(function(element) {
+            document.querySelectorAll("#iise-delivery-1 .saved-item .saved-item-name").forEach(function(element) {
                 var link = $(element).closest(".saved-item-info-link");
-                console.log(link);
+                var url = (new URL(link[0].href)).pathname;
+                var exists = metadata.saved.indexOf(url);
+                if (exists !== -1){$(element).closest(".saved-item").css("display","flex");}
             });
         }
     }
